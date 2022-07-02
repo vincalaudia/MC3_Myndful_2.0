b0f70bdab2a9eaa5bda5218e7fd1f3868c38ae3a
Angellee <angelriapurnamasari17@gmail.com>
2022-07-02 17:05:47 +0700

    feat: added durationView

--- MC2-Mindful-App/Questions/DurationView.swift
+++ MC2-Mindful-App/Questions/DurationView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct DurationView: View {
+    private var images = ["MC2-ok","MC2-not-ok"]
+    private var gridItemLayout = [GridItem(.flexible()), GridItem(.flexible())];
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                VStack() {
+                    Image("MC2")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                Spacer()
+         
+                GeometryReader { geo in
+                    VStack {
+                        Text("Apakah kamu punya waktu lebih dari 2 menit ?")
+                            .font(.system(size: 20))
+                            .padding(.top, geo.size.height * 0.09)
+                            .padding(.bottom, geo.size.height * 0.08)
+                            LazyVGrid(columns: gridItemLayout, spacing: 20) {
+                                ForEach((0..<images.count), id: \.self) { i in
+
+                                    Image(images[i])
+                                        .resizable()
+                                        .frame(width: geo.size.width * 0.15, height: geo.size.height * 0.15)
+                                }
+                            }
+                    }
+                }
+                
+            }
+        }
     }
 }
 
