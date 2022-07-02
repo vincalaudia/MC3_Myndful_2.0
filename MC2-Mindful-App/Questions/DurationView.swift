22aa609049459d11dfb6c94dec9efa6950d73e88
Angellee <angelriapurnamasari17@gmail.com>
2022-07-02 17:05:33 +0700

    feat: added emotionView

---
+++ MC2-Mindful-App/Questions/DurationView.swift
@@ -0,0 +1,20 @@
+//
+//  DurationView.swift
+//  MC2-Mindful-App
+//
+//  Created by Angel Ria Purnamasari on 02/07/22.
+//
+
+import SwiftUI
+
+struct DurationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DurationView_Previews: PreviewProvider {
+    static var previews: some View {
+        DurationView()
+    }
+}
