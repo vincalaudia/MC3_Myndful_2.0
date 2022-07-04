3b7be0282a9b339f582e3b591ab6ec3720e873bf
Bens <bennysinaga28@gmail.com>
2022-07-04 17:15:08 +0700

    Add onboarding view

---
+++ MC2-Mindful-App/OnBoardingSection/OnBoarding.swift
@@ -0,0 +1,20 @@
+//
+//  OnBoarding.swift
+//  MC2-Mindful-App
+//
+//  Created by Benni L M Sinaga on 04/07/22.
+//
+
+import SwiftUI
+
+struct OnBoarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnBoarding_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoarding()
+    }
+}
