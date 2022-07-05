27df9d9b6c7ee9d17538e37e84640988e500cc77
Bens <bennysinaga28@gmail.com>
2022-07-05 09:58:22 +0700

    Add Notes and screen 2

---
+++ MC2-Mindful-App/Mindful-Writing/mindfulWriting2.swift
@@ -0,0 +1,20 @@
+//
+//  mindfulWriting2.swift
+//  MC2-Mindful-App
+//
+//  Created by Benni L M Sinaga on 05/07/22.
+//
+
+import SwiftUI
+
+struct mindfulWriting2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct mindfulWriting2_Previews: PreviewProvider {
+    static var previews: some View {
+        mindfulWriting2()
+    }
+}
