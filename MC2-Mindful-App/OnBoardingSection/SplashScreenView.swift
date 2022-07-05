837cc62dc9f0092225c1ade68a8206f0ab82916a
Bens <bennysinaga28@gmail.com>
2022-07-05 07:54:38 +0700

    add splashscreen

---
+++ MC2-Mindful-App/OnBoardingSection/SplashScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreenView.swift
+//  MC2-Mindful-App
+//
+//  Created by Benni L M Sinaga on 05/07/22.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
