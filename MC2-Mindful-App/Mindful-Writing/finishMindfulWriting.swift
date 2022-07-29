1c6ef605da083effae42c00764ea66e30b76b9d3
Bens <bennysinaga28@gmail.com>
2022-07-29 16:03:46 +0700

    Add mindfull writing

---
+++ MC2-Mindful-App/Mindful-Writing/finishMindfulWriting.swift
@@ -0,0 +1,20 @@
+//
+//  finishMindfulWriting.swift
+//  MC2-Mindful-App
+//
+//  Created by Benni L M Sinaga on 29/07/22.
+//
+
+import SwiftUI
+
+struct finishMindfulWriting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct finishMindfulWriting_Previews: PreviewProvider {
+    static var previews: some View {
+        finishMindfulWriting()
+    }
+}
---
+++ MC2-Mindful-App/Mindful-Writing/mindfulWriting1.swift
@@ -0,0 +1,20 @@
+//
+//  mindfulWriting1.swift
+//  MC2-Mindful-App
+//
+//  Created by Benni L M Sinaga on 29/07/22.
+//
+
+import SwiftUI
+
+struct mindfulWriting1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct mindfulWriting1_Previews: PreviewProvider {
+    static var previews: some View {
+        mindfulWriting1()
+    }
+}
